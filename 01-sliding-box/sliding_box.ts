2137657f3e2324648cab240b4e5ab64fd2f94c0b
Atanas Ormanov <atanasormanow@gmail.com>
2022-10-31 20:52:14 +0200

    Keep visited states to avoid loops

--- 01-sliding-box/sliding_box.ts
+++ 01-sliding-box/sliding_box.ts
@@ -8,33 +8,10 @@ type BoxState = {
   path: string[];
 };
 
-// generte array [from..to]
-function range(from: number, to: number): number[] {
-  return Array.from(new Array(to - from + 1), (_, i) => i + from);
-}
-
-// Use range to generate the goal state
-function generateGoalBoard(side: number): number[][] {
-  let goal = range(0, side - 1).map((i) => range(side * i + 1, side * (i + 1)));
-  goal[side - 1][side - 1] = 0;
-  return goal;
-}
-
-// NOTE: Here arrays are serialized before comparison
-//       (which seems like a slow aproach)
-function isDone(node: BoxState) {
-  return (
-    JSON.stringify(node.board) ===
-    JSON.stringify(generateGoalBoard(node.board.length))
-  );
-}
-
 // A fast way to find the tile position in the goal state
 function tileToGoalPosition(tile: number, side: number): [number, number] {
-  if (tile === 0) {
-    return [side - 1, side - 1];
-  }
-  return [Math.floor(tile / side), (tile + side - 1) % side];
+  if (tile === 0) return [side - 1, side - 1];
+  else return [Math.ceil(tile / side) - 1, (tile + side - 1) % side];
 }
 
 // Heuristic function for Manhattan distance
@@ -47,7 +24,7 @@ function h({ board: board }: BoxState): number {
       const tileVal = board[i][j];
       const [i0, j0] = tileToGoalPosition(tileVal, side);
 
-      if (tileVal !== Math.pow(side, 2)) {
+      if (tileVal !== 0) {
         totalDistance += Math.abs(i - i0) + Math.abs(j - j0);
       }
     }
@@ -56,7 +33,10 @@ function h({ board: board }: BoxState): number {
   return totalDistance;
 }
 
-function possibleSlides(node: BoxState): BoxState[] {
+function possibleSlides(
+  node: BoxState,
+  visitedBoards: number[][][]
+): BoxState[] {
   const [bx, by] = node.blankPosition;
 
   // The direction of the moved tile is the dual
@@ -68,33 +48,26 @@ function possibleSlides(node: BoxState): BoxState[] {
     left: [0, 1],
   };
 
-  const dualDir = {
-    up: "down",
-    down: "up",
-    left: "right",
-    right: "left",
-  };
-
   return _.flatMap(_.keys(slides), (slideDir) => {
     const [dx, dy] = slides[slideDir];
     const [bdx, bdy] = [bx + dx, by + dy];
 
     const isValidAdj = [bdx, bdy].every((x) => _.inRange(x, node.board.length));
-    const isDualOfPred = node.path[node.path.length - 1] === dualDir[slideDir];
 
-    if (isValidAdj && !isDualOfPred) {
+    if (isValidAdj) {
       let moved = _.cloneDeep(node.board);
 
       moved[bx][by] = moved[bdx][bdy];
       moved[bdx][bdy] = 0;
 
+      const wasVisited = _.some(visitedBoards, (vb) => _.isEqual(vb, moved));
+      if (wasVisited) return [];
+
       const pathSucc = _.clone(node.path);
       pathSucc.push(slideDir);
 
       return [{ board: moved, blankPosition: [bdx, bdy], path: pathSucc }];
-    } else {
-      return [];
-    }
+    } else return [];
   });
 }
 
@@ -103,37 +76,39 @@ function IDAS(start: BoxState) {
   let treshold = h(start);
 
   while (result === null) {
-    result = limitedAStar(start, isDone, possibleSlides, treshold);
+    console.log("HERE WE GO AGAIN!");
+    result = limitedAStar(start, possibleSlides, treshold);
+    // moves most often change the h value with 2
     treshold += 1;
   }
 
   return result;
 }
 
-function limitedAStar(
-  root: BoxState,
-  isDone: Function,
-  expand: Function,
-  treshold: number
-) {
+function limitedAStar(root: BoxState, expand: Function, treshold: number) {
   let stack = [root];
+  let visitedBoards = [];
 
   while (stack.length > 0) {
     // console.log("stack: ", stack);
 
-    const current = stack[stack.length - 1];
-    if (isDone(current)) {
-      return stack.pop().path;
+    // if i keep current in the stack, ill have the path
+    // in the stack at the end, but that will cost some memory
+    const current = stack.pop();
+    if (h(current) === 0) {
+      return current.path;
     }
+    visitedBoards.push(current.board);
 
-    // sort in reversed order (top of stack = end of list)
-    const children = expand(current).sort((a: BoxState, b: BoxState) =>
-      a.path.length + h(a) >= a.path.length + h(b) ? -1 : 1
-    );
+    console.log(h(current) + current.path.length, current.board);
 
-    if (children.length === 0 || h(current) > treshold) {
-      stack.pop();
-    } else {
+    if (h(current) + current.path.length <= treshold) {
+      // console.log(`Значи твърдиш че ${h(current)} + ${current.path.length} <= ${treshold}`);
+      // sort in reversed order (top of stack = end of list)
+      const children = expand(current, visitedBoards).sort(
+        (a: BoxState, b: BoxState) =>
+          a.path.length + h(a) >= a.path.length + h(b) ? -1 : 1
+      );
       children.forEach((node: BoxState) => stack.push(node));
     }
   }
@@ -192,4 +167,5 @@ async function main() {
 
   cli.close();
 }
+
 main();
