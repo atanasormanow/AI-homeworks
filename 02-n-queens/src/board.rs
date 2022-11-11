f994e1e38a85c3be12996cdc8b7d1e8b91be549a
Atanas Ormanov <atanasormanow@gmail.com>
2022-11-11 23:59:50 +0200

    Change board state initialization

--- 02-n-queens/src/board.rs
+++ 02-n-queens/src/board.rs
@@ -1,6 +1,7 @@
 use rand::distributions::{Distribution, Uniform};
 use rand::rngs::ThreadRng;
 use rand::seq::SliceRandom;
+use std::time::Instant;
 
 #[derive(Debug)]
 pub struct BoardState {
@@ -12,46 +13,22 @@ pub struct BoardState {
 }
 
 impl BoardState {
-  pub fn is_done(board: &BoardState) -> bool {
-    board
+  pub fn is_done(&self) -> bool {
+    self
       .queens_per_col
       .iter()
-      .chain(board.queens_per_d1.iter())
-      .chain(board.queens_per_d2.iter())
+      .chain(self.queens_per_d1.iter())
+      .chain(self.queens_per_d2.iter())
       .all(|&queens_on_line| queens_on_line < 2)
   }
 
-  fn diagonal_indexes(queens: &Vec<usize>, r: usize, c: usize)
-    -> (usize, usize) {
-
-    let sum = c + r;
-    let shifted_diff = c + queens.len() - r - 1;
-
-    (shifted_diff, sum)
-  }
-
-  fn conflicts(board: &BoardState, r: usize, c: usize) -> i32 {
-    let (d1i, d2i): (usize, usize)
-                     = BoardState::diagonal_indexes(&board.queens, r, c);
-
-    let queens_count
-      = board.queens_per_col[c]
-      + board.queens_per_d1[d1i]
-      + board.queens_per_d2[d2i];
-
-    if c == board.queens[r] {
-      return queens_count as i32 - 3;
-    }
-    queens_count as i32
-  }
-
-  pub fn with_permutation(n: usize, rng: &mut ThreadRng) -> BoardState {
+  pub fn new_permutation(n: usize, rng: &mut ThreadRng) -> BoardState {
     let mut queens: Vec<usize> = (0..n).collect();
     queens.shuffle(rng);
 
-    let queens_per_col: Vec<i32> = [1].repeat(n);
-    let mut queens_per_d1: Vec<i32> = [0].repeat(2 * n - 1);
-    let mut queens_per_d2: Vec<i32> = [0].repeat(2 * n - 1);
+    let queens_per_col: Vec<i32> = vec![1;n];
+    let mut queens_per_d1: Vec<i32> = vec![0 ; 2 * n - 1];
+    let mut queens_per_d2: Vec<i32> = vec![0 ; 2 * n - 1];
 
     for row in 0..n {
       let (d1i, d2i) = BoardState::diagonal_indexes(&queens, row, queens[row]);
@@ -60,15 +37,35 @@ impl BoardState {
       queens_per_d2[d2i] += 1;
     }
 
-    BoardState {
-      queens,
-      queens_per_col,
-      queens_per_d1,
-      queens_per_d2,
-    }
+    BoardState {queens, queens_per_col, queens_per_d1, queens_per_d2}
   }
 
-  pub fn queen_with_max_conflicts(board: &BoardState, rng: &mut ThreadRng)
+  pub fn new_min_conflicts(n: usize, rng: &mut ThreadRng) -> BoardState {
+    let mut board: BoardState
+      = BoardState {
+        queens: vec![0; n],
+        queens_per_col: vec![0; n],
+        queens_per_d1: vec![0; 2 * n - 1],
+        queens_per_d2: vec![0; 2 * n - 1],
+      };
+
+    let now = Instant::now();
+    for row in 0..n {
+      let min_conflict_move = board.min_conflict_move(row, rng);
+      board.queens[row] = min_conflict_move;
+      let (d1i, d2i)
+        = BoardState::diagonal_indexes(&board.queens, row, min_conflict_move);
+
+      board.queens_per_col[min_conflict_move] += 1;
+      board.queens_per_d1[d1i] += 1;
+      board.queens_per_d2[d2i] += 1;
+    }
+    println!("Min conflicts initialization in : {:?}", now.elapsed());
+
+    board
+  }
+
+  pub fn queen_with_max_conflicts(&self, rng: &mut ThreadRng)
     -> usize {
 
       // Maybe use some priority queue or something
@@ -76,12 +73,12 @@ impl BoardState {
       let mut with_most_conflicts: Vec<(usize,i32)> = Vec::new();
       with_most_conflicts.push((0, i32::MIN));
 
-      let n = board.queens.len();
+      let n = self.queens.len();
 
       for i in 0..n {
         let (_, conf_max) = with_most_conflicts[0];
         let current_conflicts
-          = BoardState::conflicts(board, i, board.queens[i]);
+          = BoardState::conflicts(self, i, self.queens[i]);
 
         if current_conflicts >= conf_max{
           if current_conflicts != conf_max {
@@ -97,23 +94,18 @@ impl BoardState {
       queen
     }
 
-  pub fn min_conflict_move(
-    board: &BoardState,
-    queen: usize,
-    rng: &mut ThreadRng
-    ) -> usize {
-      // Maybe use some priority queue or something
-      // (queen, conflicts)
+  pub fn min_conflict_move(&self, queen: usize, rng: &mut ThreadRng)
+    -> usize {
       let mut with_least_conflicts: Vec<(usize,i32)> = Vec::new();
       with_least_conflicts.push((0, i32::MAX));
 
-      let n = board.queens.len();
+      let n = self.queens.len();
 
       for c in 0..n {
         let (_, conf_min) = with_least_conflicts[0];
-        let current_conflicts = BoardState::conflicts(board, queen, c);
+        let current_conflicts = self.conflicts(queen, c);
 
-        if current_conflicts <= conf_min{
+        if current_conflicts <= conf_min {
           if current_conflicts != conf_min {
             with_least_conflicts.clear();
           }
@@ -127,30 +119,30 @@ impl BoardState {
       destination
     }
 
-  pub fn move_queen(board: &mut BoardState, queen: usize, destination: usize) {
-    let queen_col = board.queens[queen];
+  pub fn move_queen(&mut self, queen: usize, destination: usize) {
+    let queen_col = self.queens[queen];
     let (queen_d1i, queen_d2i)
-      = BoardState::diagonal_indexes(&board.queens, queen, queen_col);
+      = BoardState::diagonal_indexes(&self.queens, queen, queen_col);
     let (dest_d1i, dest_d2i)
-      = BoardState::diagonal_indexes(&board.queens, queen, destination);
+      = BoardState::diagonal_indexes(&self.queens, queen, destination);
 
-    board.queens[queen] = destination;
+    self.queens[queen] = destination;
 
-    board.queens_per_col[queen_col] -= 1;
-    board.queens_per_col[destination] += 1;
+    self.queens_per_col[queen_col] -= 1;
+    self.queens_per_col[destination] += 1;
 
-    board.queens_per_d1[queen_d1i] -= 1;
-    board.queens_per_d1[dest_d1i] += 1;
+    self.queens_per_d1[queen_d1i] -= 1;
+    self.queens_per_d1[dest_d1i] += 1;
 
-    board.queens_per_d2[queen_d2i] -= 1;
-    board.queens_per_d2[dest_d2i] += 1;
+    self.queens_per_d2[queen_d2i] -= 1;
+    self.queens_per_d2[dest_d2i] += 1;
   }
 
-  pub fn print_board(board: &BoardState) {
-    let n = board.queens.len();
+  pub fn print_board(&self) {
+    let n = self.queens.len();
     for r in 0..n {
       for c in 0..n {
-        if c == board.queens[r] {
+        if c == self.queens[r] {
           print!("*");
         } else {
           print!("-");
@@ -159,4 +151,30 @@ impl BoardState {
       println!();
     }
   }
+
+  // Private Functions:
+  //-------------------
+  fn diagonal_indexes(queens: &Vec<usize>, r: usize, c: usize)
+    -> (usize, usize) {
+
+    let sum = c + r;
+    let shifted_diff = c + queens.len() - r - 1;
+
+    (shifted_diff, sum)
+  }
+
+  fn conflicts(&self, r: usize, c: usize) -> i32 {
+    let (d1i, d2i) = BoardState::diagonal_indexes(&self.queens, r, c);
+
+    let queens_count
+      = self.queens_per_col[c]
+      + self.queens_per_d1[d1i]
+      + self.queens_per_d2[d2i];
+
+    if c == self.queens[r] {
+      return queens_count as i32 - 3;
+    }
+    queens_count as i32
+  }
+
 }
--- 02-n-queens/src/main.rs
+++ 02-n-queens/src/main.rs
@@ -8,17 +8,20 @@ use board::BoardState;
 fn min_conflicts(max_steps: usize, n: usize, rng: &mut ThreadRng)
   -> Option<BoardState>{
 
-  let mut board: BoardState = BoardState::with_permutation(n, rng);
+  let init_now = Instant::now();
+  let mut board: BoardState = BoardState::new_min_conflicts(n, rng);
+  println!("Initialized the board in {:?}!", init_now.elapsed());
+
+  let now = Instant::now();
 
   for i in 0..max_steps {
-    BoardState::print_board(&board);
-    if BoardState::is_done(&board) {
-      println!("Just found a solution in {i} steps!");
+    if board.is_done() {
+      let elapsed = now.elapsed();
+      println!("Just found a solution in {:?} steps and {:?}!", i, elapsed);
       return Some(board);
     }
 
-    let max_conf_queen
-      = BoardState::queen_with_max_conflicts(&board, rng);
+    let max_conf_queen = board.queen_with_max_conflicts(rng);
     let min_conf_tile
       = BoardState::min_conflict_move(&board, max_conf_queen, rng);
 
@@ -30,13 +33,11 @@ fn min_conflicts(max_steps: usize, n: usize, rng: &mut ThreadRng)
 
 fn main() {
   let mut rng: ThreadRng = rand::thread_rng();
-  let max_steps = 100;
+  let max_steps = 200;
 
   let mut user_input = String::new();
   let stdin = io::stdin(); // We get `Stdin` here.
   stdin.read_line(&mut user_input).ok();
-
-  let now = Instant::now();
 
   let result
     = user_input
@@ -45,15 +46,14 @@ fn main() {
     .ok()
     .and_then(|n| min_conflicts(max_steps, n, &mut rng));
 
-  println!("Finished in: {:?}", now.elapsed());
-
-  match result {
-    Some(board) => BoardState::print_board(&board),
-    None => println!("Solition not found :(")
-  }
+  // match result {
+  //   Some(board) => BoardState::print_board(&board),
+  //   None => println!("Solition not found :(")
+  // }
 }
 
 // TODO:
 // - use minConflicts to init the board
 // - introduce random restart, to avoid loops
 // - try to make the board module cleaner
+// - time execution without including board initialization
