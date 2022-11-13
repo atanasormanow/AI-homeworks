139ef30fbd025a0b7688f338764b4380ba60664e
Atanas Ormanov <atanasormanow@gmail.com>
2022-11-13 12:57:24 +0200

    Add loop with treshold and don't return Option<>

--- 02-n-queens/src/main.rs
+++ 02-n-queens/src/main.rs
@@ -6,50 +6,44 @@ mod board;
 use board::BoardState;
 
 fn min_conflicts(max_steps: usize, n: usize, rng: &mut ThreadRng)
-  -> Option<BoardState>{
+  -> BoardState{
 
-  let now = Instant::now();
+    let now = Instant::now();
 
-  let mut board: BoardState = BoardState::new_min_conflicts(n, rng);
+    loop {
+      let mut board: BoardState = BoardState::new_min_conflicts(n, rng);
 
-  for i in 0..max_steps {
-    if board.is_done() {
-      println!(
-        "Just found a solution in {i} steps and {:?}!",
-        now.elapsed()
-        );
-      return Some(board);
+      for i in 0..max_steps {
+        if board.is_done() {
+          println!(
+            "Just found a solution in {i} steps and {:?}!",
+            now.elapsed()
+            );
+          return board;
+        }
+
+        let max_conf_queen = board.queen_with_max_conflicts(rng);
+        let min_conf_tile = board.min_conflict_move(max_conf_queen, rng);
+
+        board.move_queen(max_conf_queen, min_conf_tile);
+      }
     }
-
-    let max_conf_queen = board.queen_with_max_conflicts(rng);
-    let min_conf_tile = board.min_conflict_move(max_conf_queen, rng);
-
-    board.move_queen(max_conf_queen, min_conf_tile);
   }
-
-  None
-}
 
 fn main() {
   let mut rng: ThreadRng = rand::thread_rng();
   let max_steps = 200;
 
   let mut user_input = String::new();
-  let stdin = io::stdin(); // We get `Stdin` here.
+  let stdin = io::stdin();
+  println!("Enter board size (number of queens):");
   stdin.read_line(&mut user_input).ok();
 
-  let result
-    = user_input
-    .trim_end()
-    .parse::<usize>()
-    .ok()
-    .and_then(|n| min_conflicts(max_steps, n, &mut rng));
+  let n = user_input.trim_end().parse::<usize>();
 
-  match result {
-    Some(board) => board.print_board(),
-    None => println!("Solition not found :(")
+  match n {
+    Ok(n)
+      => min_conflicts(max_steps, n, &mut rng).print_board(),
+    Err(e) => println!("{:?}", e),
   }
 }
-
-// TODO:
-// - introduce random restart, to avoid loops
