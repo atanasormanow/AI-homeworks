789e33709a97cdf74c26ce2925b61075a32072fc
Atanas Ormanov <atanasormanow@gmail.com>
2022-11-18 17:09:06 +0200

    Add WIP version of TSP in Haskell

--- travelling-salesman-hs/app/Main.hs
+++ travelling-salesman-hs/app/Main.hs
@@ -1,12 +1,11 @@
 module Main (main) where
 
+import Control.Monad (replicateM)
 import Data.List (sortBy, (\\))
-import Data.Vector hiding ((++))
+import Data.Vector hiding (drop, take)
 import Immutable.Shuffle (shuffleM)
-import Lib
 import System.Random (getStdRandom, randomR)
 import Prelude hiding (head, tail)
-import Control.Monad (replicateM)
 
 type VIndividual = Vector Int
 
@@ -19,18 +18,20 @@ type Point = (Int, Int)
 type Pair = (VIndividual, VIndividual)
 
 data Selection = Selection [VIndividual] [ScoredVIndividual]
+  deriving (Show)
 
--- IO
+-- -- IO
 generatePoints :: Int -> IO (Vector Point)
 generatePoints n =
   let genPoint = getStdRandom $ randomR ((-n, -n), (n, n))
    in Data.Vector.replicateM n (genPoint :: IO Point)
 
-newVIndividual :: Int -> IO VIndividual
-newVIndividual n = (shuffleM . fromList) [0 .. n-1]
+permutationOf :: Int -> Int -> IO VIndividual
+permutationOf m n = (shuffleM . fromList) [m .. n]
 
 newGeneration :: Int -> Int -> IO [VIndividual]
-newGeneration n = flip Control.Monad.replicateM (newVIndividual n)
+newGeneration n = flip Control.Monad.replicateM $ permutationOf 0 (n - 1)
+
 -----
 
 distance :: Point -> Point -> Float
@@ -39,7 +40,6 @@ distance (x1, y1) (x2, y2) =
       dy = y2 - y1
    in (sqrt . fromIntegral) (dx * dx + dy * dy)
 
--- TODO: test
 score :: Vector Point -> VIndividual -> Fitness
 score ps inds =
   let op (d, i') i = (d + distance (ps ! i) (ps ! i'), i)
@@ -60,40 +60,51 @@ vdiff v1 v2 =
 evalFitness :: Vector Point -> [VIndividual] -> [ScoredVIndividual]
 evalFitness ps = Prelude.map (\v -> (v, score ps v))
 
--- TODO: think of a better 10
+sortGeneration :: [ScoredVIndividual] -> [ScoredVIndividual]
+sortGeneration inds =
+  let totalScore = Prelude.foldl (\acc (_, s) -> acc + s) 0 inds
+      cmp (_, f1) (_, f2) = compare (f1 / totalScore) (f2 / totalScore)
+   in sortBy cmp inds
+
 select :: [ScoredVIndividual] -> Selection
 select xs =
-  let totalScore = Prelude.foldl (\acc (_, s) -> acc + s) 0 xs
-      (top70, bot30) =
-        (Prelude.splitAt 70 . sortBy (flip (\(_, f1) (_, f2) -> compare f1 f2))) xs
+  let sorted = sortGeneration xs
+      (top70, bot30) = Prelude.splitAt 70 sorted
       new = Prelude.map fst top70
-      -- concat with the shorter list on the left
-      old = Prelude.drop 20 bot30 ++ Prelude.take 20 top70
+      old = take 20 top70 Prelude.++ drop 20 bot30
    in Selection new old
 
+-- TODO: maybe change types
+splitPair :: Pair -> (Pair, IO Int)
+splitPair p@(ind1, _) =
+  let n = Data.Vector.length ind1
+      rIndex = getStdRandom $ randomR (0, n - 1)
+   in (p, rIndex)
 
 crossOp :: Int -> Pair -> Pair
-crossOp i (parent1,parent2) =
-  let (pref1, suff1) = Data.Vector.splitAt i parent1
-      (pref2, suff2) = Data.Vector.splitAt i parent2
-      child1 = pref1 +v+ vdiff parent2 pref1
-      child2 = pref2 +v+ vdiff parent1 pref2
-   in (fromList [], fromList [])
+crossOp i (parent1, parent2) =
+  let (pref1, _) = Data.Vector.splitAt i parent1
+      (pref2, _) = Data.Vector.splitAt i parent2
+      child1 = pref1 Data.Vector.++ vdiff parent2 pref1
+      child2 = pref2 Data.Vector.++ vdiff parent1 pref2
+   in (child1, child2)
 
 -- TODO
-cross :: [VIndividual] -> [VIndividual] -> [Pair]
-cross = Prelude.zipWith (crossOp 5)
+cross :: [(Pair, IO Int)] -> [Pair]
+cross = undefined
 
-mutate :: [VIndividual] -> [VIndividual]
-mutate = undefined
+swap :: VIndividual -> (Int, Int) -> VIndividual
+swap ind (i,j)= ind // [(i, ind ! j), (j, ind ! i)]
+
+-- mutate :: [VIndividual] -> (Vector Int, Vector Int) -> [VIndividual]
+-- mutate inds (is, js) = Prelude.zipWith (\ind xy -> swap ind xy)
 
 main :: IO ()
 main = do
+  gen <- newGeneration 5 10
   points <- generatePoints 5
-  print points
-  individual <- newVIndividual 5
-  print individual
-  print $ score points individual
+  let scored = evalFitness points gen
+   in print $ sortGeneration scored
 
 -- TODO:
 -- - take user input
@@ -103,3 +114,4 @@ main = do
 --    where n is the number of nodes (number of genes an individual has)
 -- - selection proportions: cross 70to -> 70new; 30new = 20to + 10bo
 --    (ill assume i have 100 individuals for now)
+-- - mutate using zipWith, permutationOf
