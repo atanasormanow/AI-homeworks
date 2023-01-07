d8ac92be3c902b6cf027d1397629bce19b1b958d
Atanas Ormanov <atanasormanow@gmail.com>
2023-01-07 12:46:50 +0200

    Add k-means homework

---
+++ 07-k-means/k_means.ts
@@ -0,0 +1,99 @@
+import _ from "lodash";
+import { readFile } from "fs/promises";
+
+type Point = {
+  x: number,
+  y: number
+}
+
+type Centroid = Point;
+
+type Cluster = {
+  centroid: Centroid,
+  points: Point[]
+}
+
+function parseDataPoints(content: string): Point[] {
+  return content
+    .split('\n')
+    .map(entry => {
+      const sep = entry.split('\t')
+      return { x: +sep[0], y: +sep[1] }
+    });
+}
+
+// Find the new centroid location for a cluster
+function updateCentroid({ points: p }: Cluster): Centroid {
+  const [xsum, ysum] =
+    _.reduce(p, ([xs, ys], { x, y }) => [xs + x, ys + y], [0, 0]);
+  return { x: xsum / p.length, y: ysum / p.length };
+}
+
+// find the closest centroid to a given point
+function findClosestCentroid(centroids: Centroid[], { x, y }: Point): Centroid {
+  const [_d, cent]: [number, Point] =
+    _.reduce(centroids,
+      ([d, c0], { x: x1, y: y1 }) => {
+
+        const currentDistance
+          = Math.sqrt(Math.pow(x - x1, 2) + Math.pow(y - y1, 2))
+
+        if (currentDistance < d) {
+          return [currentDistance, { x: x1, y: y1 }];
+        } else return [d, c0];
+      },
+      [0, { x, y }]); // Default value is the point itself
+  return cent;
+}
+
+// TODO
+function formClusters(centroids: Centroid[], points: Point[]): Cluster[] {
+  // const matchToClosestCentroid
+  //   = points.map(p => [p, findClosestCentroid(centroids, p)]);
+
+  const newClusters =
+    _.groupBy(points, p => findClosestCentroid(centroids, p));
+  console.log(newClusters);
+  return [];
+}
+
+function getRandomFloat(min: number, max: number, decimals: number) {
+  const str = (Math.random() * (max - min) + min).toFixed(decimals);
+  return parseFloat(str);
+}
+
+function kRandomCentroids(k: number): Centroid[] {
+  return _.range(k).map(_x => {
+    return {
+      x: getRandomFloat(1, 10, 3),
+      y: getRandomFloat(1, 10, 3)
+    }
+  });
+}
+
+// Hint: isEqual
+function step(clusters: Cluster[], points: Point[]): Cluster[] {
+  // Update cluster locations to the avg of its points
+  const newCentroids: Centroid[] = clusters.map(updateCentroid);
+
+  // Update which points belong to which cluster
+  // TODO: determine type
+  const newClusters = _.groupBy(points, p => findClosestCentroid(newCentroids, p));
+  console.log(newClusters);
+  return [];
+}
+
+async function main() {
+  readFile("static/normal.txt").then(content => {
+    const points = parseDataPoints(content.toString());
+    const centroids = kRandomCentroids(5);
+    const clusters = formClusters(centroids, points);
+    console.log(clusters);
+
+  }).catch(error => {
+    console.error(error.message);
+    process.exit(1);
+  });
+}
+
+main();
